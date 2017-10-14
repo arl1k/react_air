1de9b7c5b9207f235911b79128d46deb56373d38
Viktor <lubarskyvik@gmail.com>
2017-10-15 01:11:02 +0300

    "Access-Control-Allow-Origin" added

--- src/logic.js
+++ src/logic.js
@@ -4,6 +4,7 @@ var options = {
     method: 'GET',
     headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+        "Access-Control-Allow-Origin": "*"
     }
 }
 
